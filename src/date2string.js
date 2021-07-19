fb3952b569ecdb7c8eda03619addcd13b50042e6
thezeeck <estigma.e@gmail.com>
2021-07-19 18:05:01 -0500

    map function
    change for each to map function

--- src/date2string.js
+++ src/date2string.js
@@ -1,16 +1,16 @@
-String.prototype.customFormatDate = function() {
+String.prototype.customFormatDate = function () {
     let oldDate = this,
         pattern = /^([1-9]|0[1-9]|1[012])[\/]([1-9]|0[1-9]|[12][0-9]|3[01])[\/](19|20)\d\d$/;
 
-    if (pattern.test(oldDate)) {
-        oldDate = oldDate.split("/");
-    } else {
+    if (!pattern.test(oldDate)) {
         console.error("Format not supported");
         return null
     }
-    oldDate.forEach((element, index) => {
-        oldDate[index] = element.length > 1 ? oldDate[index] : `0${oldDate[index]}`
-    });
+
+    oldDate = oldDate.split("/");
+
+    oldDate.map((element) => element.length > 1 ? element : `0${element}`);
+
     return `${oldDate[2]}${oldDate[0]}${oldDate[1]}`;
 }
 
